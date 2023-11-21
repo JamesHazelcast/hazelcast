7e9b8b75e895226da08bd7545361c24a67cb04bf
Jack <jack.green@hazelcast.com>
2023-11-21 14:48:02 +0000

    Organise imports

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/task/dynamicconfig/AbstractUpdateConfigMessageTask.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/task/dynamicconfig/AbstractUpdateConfigMessageTask.java
@@ -28,7 +28,6 @@ import com.hazelcast.security.permission.ConfigPermission;
 
 import java.security.Permission;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
