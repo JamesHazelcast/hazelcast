f5c45386d5941252285839fa4f467cf73f865e02
Gareth Johnston <gareth.johnston@hazelcast.com>
2023-11-22 11:04:59 +0000

    Add IMap MapEvictedListener test

---
+++ hazelcast/src/test/java/com/hazelcast/internal/namespace/imap/IMapMapEvictedListenerUCDTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.internal.namespace.imap;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EntryListenerConfig;
+import com.hazelcast.map.IMap;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IMapMapEvictedListenerUCDTest extends IMapUCDTest {
+    @Override
+    public void test() throws Exception {
+        populate();
+        map.evictAll();
+        IMap<String, Boolean> map = instance.getMap("IMapMapEvictedListenerUCDTest");
+        assertTrueEventually(() -> {
+            Boolean evicted = map.get("mapEvicted");
+            assertNotNull(evicted);
+            assertTrue(evicted);
+        });
+    }
+
+    @Override
+    protected void mutateConfig(Config config) {
+        EntryListenerConfig entryListenerConfig = new EntryListenerConfig();
+        entryListenerConfig.setClassName(getUserDefinedClassNames()[0]);
+        mapConfig.setEntryListenerConfigs(List.of(entryListenerConfig));
+
+        super.mutateConfig(config);
+    }
+
+    @Override
+    protected String[] getUserDefinedClassNames() {
+        return new String[] {"usercodedeployment.IMapMapEvictedListener"};
+    }
+}
